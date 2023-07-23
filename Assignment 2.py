4bf127f5e146b411208d6c20118227087f5965bd
Abhijith0002 <126601807+Abhijith0002@users.noreply.github.com>
2023-07-23 01:17:27 -0700

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -70,7 +70,7 @@ for i in range(1,51):
 print (l)
 
 """
-
+"""
 # 8. program to count even and odd number in a list
 
 l = []
@@ -85,3 +85,98 @@ for i in l:
         odd.append(i)     
 print (len(even))
 print (len(odd))
+
+"""
+"""
+
+# 9. Write a python program to remove zeros from an IP address("216.08.094.196")
+
+ip_address = input("Enter an IP address: ")
+inp = ip_address.split('.')
+
+for i in range((len(inp))):
+    inp[i] = inp[i].lstrip('0')
+ip_address = '.'.join(inp)
+
+print (ip_address)
+
+"""
+"""
+
+# 10 Write a Python program that matches a string that has an 'a' followed by anything. 
+
+import re
+pattern = re.compile('a*')
+
+string = input('Enter a string: ')
+
+if pattern.match(string):
+    print('The string contains an "a" followed by anything')
+else:
+    print('The string does not contain an "a" followed by anything')
+
+"""
+"""
+
+# 11 Replace all occurences of 6 with 'six' and 10 with 'ten' for the given string 'They ate 6 apples and 10 banana'
+
+import re
+input = input ("Enter a string with 6 and 10:")
+a = re.sub("6","six",input) 
+b = re.sub("10","ten",a)
+print (b)
+
+"""
+"""
+
+# 12.	Write a program to check whether a person is eligible for voting or not. (accept age from user)
+
+def check_age():
+    if (age < 18 or age > 60):
+        print ("You are not eligible for voting..\U0001F62D ")
+    else:
+        print ("You are eligible for voting..\U0001f600")
+
+while True:
+    print ('''
+    ELIGIBILITY CHECK
+    ''')
+    age = int (input("Enter your Age: "))
+    check_age()
+
+"""
+"""
+#. Write a program to calculate the electricity bill (accept number of unit from user) according to the following criteria : 
+# First 100 units                                               no charge
+# Next 100 units                                              Rs 5 per unit
+# After 200 units                                             Rs 10 per unit
+# (For example if input unit is 350 than total bill amount is Rs2000)
+
+units = int(input("Enter the number of units: "))
+total_bill = 0
+remaining_units = units
+
+if remaining_units >= 100:
+    total_bill += 0
+    remaining_units -= 100
+else:
+    total_bill += remaining_units * 0
+    remaining_units = 0
+
+if remaining_units >= 100:
+    total_bill += 100 * 5
+    remaining_units -= 100
+else:
+    total_bill += remaining_units * 5
+    remaining_units = 0
+
+if remaining_units > 0:
+    total_bill += remaining_units * 10
+
+print(f"Total bill amount is Rs {total_bill}")
+
+"""
+
+
+
+
